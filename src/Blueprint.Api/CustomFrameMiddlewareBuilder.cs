afaf372f92f37a4a6a6127eba081256b20f5b08c
Adam Barclay <barclay.adam@gmail.com>
2019-10-04 14:55:22 +0100

    [api] Add a simple helper for single-frame custom middleware builders, use in ApplicationInsightsMiddleware

---
+++ src/Blueprint.Api/CustomFrameMiddlewareBuilder.cs
@@ -0,0 +1,30 @@
+using Blueprint.Compiler.Frames;
+
+namespace Blueprint.Api
+{
+    /// <summary>
+    /// Provides a simple base class for implementing custom <see cref="IMiddlewareBuilder"/>s that
+    /// require a single custom <see cref="Frame" /> to be appended to the generated method.
+    /// </summary>
+    public abstract class CustomFrameMiddlewareBuilder : Frame, IMiddlewareBuilder
+    {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="CustomFrameMiddlewareBuilder" /> class.
+        /// </summary>
+        /// <param name="isAsync">Whether this frame has async code within (i.e. depends on await)</param>
+        protected CustomFrameMiddlewareBuilder(bool isAsync) : base(isAsync)
+        {
+        }
+
+        public MiddlewareBuilderContext BuilderContext { get; private set; }
+
+        public abstract bool Matches(ApiOperationDescriptor operation);
+
+        public void Build(MiddlewareBuilderContext context)
+        {
+            BuilderContext = context;
+
+            context.AppendFrames(this);
+        }
+    }
+}
