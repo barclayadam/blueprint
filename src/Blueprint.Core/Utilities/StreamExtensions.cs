0b18f67880bd1162135dc44369dbc90439c03132
Adam Barclay <barclay.adam@gmail.com>
2019-10-06 12:28:15 +0100

    [utils] Add some useful Steam & DateTime extensions

--- src/Blueprint.Core/Utilities/StreamExtensions.cs
+++ src/Blueprint.Core/Utilities/StreamExtensions.cs
@@ -1,4 +1,5 @@
 using System.IO;
+using System.Threading.Tasks;
 
 namespace Blueprint.Core.Utilities
 {
@@ -37,6 +38,29 @@ namespace Blueprint.Core.Utilities
 
                 return ms.ToArray();
             }
+        }
+
+        /// <summary>
+        /// Reads the given <see cref="Stream" /> fully, converting to a string using a new
+        /// <see cref="StreamReader"/>.
+        /// </summary>
+        /// <remarks>
+        /// <strong>IF</strong> the given input stream supports seeking, this method will
+        /// call <see cref="Stream.Seek" /> to set the position to 0.
+        /// </remarks>
+        /// <param name="input">The stream to read.</param>
+        /// <returns>The stream read as a string.</returns>
+        public static Task<string> ReadFullyAsStringAsync(this Stream input)
+        {
+            Guard.NotNull(nameof(input), input);
+
+            if (input.CanSeek)
+            {
+                input.Seek(0, SeekOrigin.Begin);
+            }
+
+            var streamReader = new StreamReader(input);
+            return streamReader.ReadToEndAsync();
         }
     }
 }
