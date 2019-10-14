ec58e022404dde68f16c5833c1fe3be6aab2a22c
Adam Barclay <barclay.adam@gmail.com>
2019-10-14 09:41:17 +0100

    [samples] Use dark (okaidia) theme for source code output

--- samples/Blueprint.Sample.WebApi/Api/SourceCodeQuery.cs
+++ samples/Blueprint.Sample.WebApi/Api/SourceCodeQuery.cs
@@ -25,15 +25,16 @@ namespace Blueprint.Sample.WebApi.Api
                 .Replace(">", "&gt;");
 
             var template = $@"
-                <!DOCTYPE html>
-                <html>
-                <body>
-	                <link href=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism.css"" rel=""stylesheet"" />
-                    <pre><code class=""language-csharp"">{codeDidIGenerate}</code></pre>
-                    <script src=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/prism.js""></script>
-                    <script src=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-csharp.js""></script>
-                </body>
-                </html>";
+<!DOCTYPE html>
+<html>
+<body>
+    <style>body {{ margin: 0 }}</style>
+	<link href=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/themes/prism-okaidia.css"" rel=""stylesheet"" />
+    <pre style=""margin: 0; border-radius: 0;""><code class=""language-csharp"">{codeDidIGenerate}</code></pre>
+    <script src=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/prism.js""></script>
+    <script src=""https://cdnjs.cloudflare.com/ajax/libs/prism/1.17.1/components/prism-csharp.js""></script>
+</body>
+</html>";
 
             var result = new PlainTextResult(template)
             {
