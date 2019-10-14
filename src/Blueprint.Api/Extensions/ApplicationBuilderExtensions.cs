b7dacca12ed87fbf5a60ac98629244fd5dfe099d
Adam Barclay <barclay.adam@gmail.com>
2019-10-14 10:29:08 +0100

    [api] Defer exception throwing in HTTP endpoint to enable much better developer exception output on compilation failure

--- src/Blueprint.Api/Extensions/ApplicationBuilderExtensions.cs
+++ src/Blueprint.Api/Extensions/ApplicationBuilderExtensions.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Net;
 using System.Threading.Tasks;
 using Blueprint.Api;
+using Blueprint.Compiler;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Routing.Constraints;
+using Microsoft.CodeAnalysis;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -20,19 +23,37 @@ namespace Microsoft.AspNetCore.Builder
             this IApplicationBuilder applicationBuilder,
             string apiPrefix)
         {
-            var options = applicationBuilder.ApplicationServices.GetRequiredService<BlueprintApiOptions>();
-            var apiDataModel = applicationBuilder.ApplicationServices.GetRequiredService<ApiDataModel>();
-            var apiOperationExecutor = applicationBuilder.ApplicationServices.GetRequiredService<IApiOperationExecutor>();
-            var inlineConstraintResolver = applicationBuilder.ApplicationServices.GetRequiredService<IInlineConstraintResolver>();
-
             // Ensure ends with a slash, but only one
             apiPrefix = apiPrefix.TrimEnd('/') + '/';
 
             var routeBuilder = new RouteBuilder(applicationBuilder);
-            var routeHandler = new BlueprintApiRouter(
-                apiOperationExecutor,
-                applicationBuilder.ApplicationServices,
-                applicationBuilder.ApplicationServices.GetRequiredService<ILogger<BlueprintApiRouter>>());
+            var apiDataModel = applicationBuilder.ApplicationServices.GetRequiredService<ApiDataModel>();
+            var inlineConstraintResolver = applicationBuilder.ApplicationServices.GetRequiredService<IInlineConstraintResolver>();
+
+            IRouter routeHandler;
+
+            try
+            {
+                var apiOperationExecutor = applicationBuilder.ApplicationServices.GetRequiredService<IApiOperationExecutor>();
+
+                routeHandler = new BlueprintApiRouter(
+                    apiOperationExecutor,
+                    applicationBuilder.ApplicationServices,
+                    applicationBuilder.ApplicationServices.GetRequiredService<ILogger<BlueprintApiRouter>>());
+            }
+            catch (CompilationException e)
+            {
+                // When a compilation exception occurs (happens when getting IApiOperationExecutor for the first time as it is
+                // registered as a singleton factory) we create a route handler that will throw a WrapperException.
+                //
+                // The WrapperException implements ICompilationException which means if we are using the developer exception
+                // page (i.e. app.UseDeveloperExceptionPage()) we get a nice compilation error page that includes the error
+                // message AND complete file source code to enable much better diagnostics of the compilation issue).
+                //
+                // The consequence of this is that the app is NOT prevented from starting up and only when hitting an API endpoint
+                // would be exception be known to HTTP clients (the error is logged regardless)
+                routeHandler = new RouteHandler(context => throw new CompilationWrapperException(e));
+            }
 
             // Ordering by 'indexOf {' means we put those URLs which are not placeholders
             // first (e.g. /users/{id} and /users/me will put /users/me first)
@@ -44,10 +65,7 @@ namespace Microsoft.AspNetCore.Builder
                     target: routeHandler,
                     routeName: link.UrlFormat + "-" + link.OperationDescriptor.HttpMethod,
                     routeTemplate: apiPrefix + safeRouteUrl,
-                    defaults: new RouteValueDictionary(new
-                    {
-                        operation = link.OperationDescriptor,
-                    }),
+                    defaults: new RouteValueDictionary(new {operation = link.OperationDescriptor,}),
                     constraints: new Dictionary<string, object>
                     {
                         ["httpMethod"] = new HttpMethodRouteConstraint(link.OperationDescriptor.HttpMethod.ToString()),
@@ -57,6 +75,45 @@ namespace Microsoft.AspNetCore.Builder
             }
 
             applicationBuilder.UseRouter(routeBuilder.Build());
+        }
+
+        private class CompilationWrapperException : Exception, ICompilationException
+        {
+            private readonly CompilationException compilationException;
+
+            public CompilationWrapperException(CompilationException compilationException) : base("Pipeline compilation failed", compilationException)
+            {
+                this.compilationException = compilationException;
+            }
+
+            public IEnumerable<CompilationFailure> CompilationFailures => compilationException.Failures
+                .Select(f =>
+                {
+                    var sourceTree = f.Location.SourceTree;
+                    var sourceCode = sourceTree.ToString();
+
+                    return new CompilationFailure(
+                        sourceTree.FilePath,
+                        sourceCode,
+                        sourceCode,
+                        new[] {ToDiagnosticMessage(f)});
+                });
+
+            private static DiagnosticMessage ToDiagnosticMessage(Diagnostic f)
+            {
+                var span = f.Location.GetLineSpan();
+                var mappedSpan = f.Location.GetMappedLineSpan();
+                var message = f.GetMessage();
+
+                return new DiagnosticMessage(
+                    message,
+                    message,
+                    f.Location.SourceTree.FilePath,
+                    mappedSpan.Span.Start.Line,
+                    span.StartLinePosition.Character,
+                    mappedSpan.Span.End.Line,
+                    span.EndLinePosition.Character);
+            }
         }
 
         private class BlueprintApiRouter : IRouter
