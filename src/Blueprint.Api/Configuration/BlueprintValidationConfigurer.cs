e8af893cc163a37396b96a554f8fb6f9030b61e0
Adam Barclay <barclay.adam@gmail.com>
2019-12-02 16:00:59 +0000

    [middleware] Add required generic param to AddAuth for context factory + some XML docs

--- src/Blueprint.Api/Configuration/BlueprintValidationConfigurer.cs
+++ src/Blueprint.Api/Configuration/BlueprintValidationConfigurer.cs
@@ -8,13 +8,21 @@ namespace Blueprint.Api.Configuration
     {
         private readonly BlueprintMiddlewareConfigurer middlewareConfigurer;
 
-        public BlueprintValidationConfigurer(BlueprintMiddlewareConfigurer middlewareConfigurer)
+        internal BlueprintValidationConfigurer(BlueprintMiddlewareConfigurer middlewareConfigurer)
         {
             this.middlewareConfigurer = middlewareConfigurer;
 
             middlewareConfigurer.Services.TryAddSingleton<IValidator, BlueprintValidator>();
         }
 
+        /// <summary>
+        /// Adds the <see cref="BlueprintValidationSourceBuilder"/> validation builder which will use any <see cref="BlueprintValidationAttribute"/>s applied
+        /// to the properties of an <see cref="IApiOperation"/>.
+        /// </summary>
+        /// <remarks>
+        /// Blueprint validation attributes are similar to DataAnnotations but have access to <see cref="ApiOperationContext" /> in addition to being
+        /// async when validating.
+        /// <returns>This configurer.</returns>
         public BlueprintValidationConfigurer UseBlueprintSource()
         {
             middlewareConfigurer.Services.AddSingleton<IValidationSource, BlueprintValidationSource>();
@@ -23,6 +31,11 @@ namespace Blueprint.Api.Configuration
             return this;
         }
 
+        /// <summary>
+        /// Adds a DataAnnotation validation builder which will use the standard DataAnnotations attributes, applied
+        /// to the properties of an <see cref="IApiOperation"/>.
+        /// </summary>
+        /// <returns>This configurer.</returns>
         public BlueprintValidationConfigurer UseDataAnnotationSource()
         {
             middlewareConfigurer.Services.AddSingleton<IValidationSource, DataAnnotationsValidationSource>();
--- src/Blueprint.Api/Configuration/BuiltinBlueprintMiddlewareConfigurers.cs
+++ src/Blueprint.Api/Configuration/BuiltinBlueprintMiddlewareConfigurers.cs
@@ -27,11 +27,28 @@ namespace Blueprint.Api.Configuration
             return middlewareConfigurer;
         }
 
+        /// <summary>
+        /// Adds validation middleware, by default adding both Blueprint and DataAnnotations sources.
+        /// </summary>
+        /// <seealso cref="BlueprintValidationConfigurer.UseBlueprintSource"/>
+        /// <seealso cref="BlueprintValidationConfigurer.UseDataAnnotationSource"/>
+        /// <param name="middlewareConfigurer">The configurer to add validation to.</param>
+        /// <returns>The configurer.</returns>
         public static BlueprintMiddlewareConfigurer AddValidation(this BlueprintMiddlewareConfigurer middlewareConfigurer)
         {
             return AddValidation(middlewareConfigurer, o => o.UseBlueprintSource().UseDataAnnotationSource());
         }
 
+        /// <summary>
+        /// Adds validation middleware, which will use "validation sources" to handle different types of validation that can be
+        /// registered against <see cref="IApiOperation" /> classes.
+        /// </summary>
+        /// <seealso cref="BlueprintValidationConfigurer.UseBlueprintSource"/>
+        /// <seealso cref="BlueprintValidationConfigurer.UseDataAnnotationSource"/>
+        /// <param name="middlewareConfigurer">The configurer to add validation to.</param>
+        /// <param name="configure">An action that will be given an instance of <see cref="BlueprintValidationConfigurer"/> to configure the validation
+        /// middleware.</param>
+        /// <returns>The configurer.</returns>
         public static BlueprintMiddlewareConfigurer AddValidation(this BlueprintMiddlewareConfigurer middlewareConfigurer, Action<BlueprintValidationConfigurer> configure)
         {
             middlewareConfigurer.AddMiddleware<ValidationMiddlewareBuilder>(MiddlewareStage.Validation);
@@ -63,9 +80,26 @@ namespace Blueprint.Api.Configuration
             return middlewareConfigurer;
         }
 
-        public static BlueprintMiddlewareConfigurer AddAuth(this BlueprintMiddlewareConfigurer middlewareConfigurer)
+        /// <summary>
+        /// Adds authentication and authorisation middlewares using the given <see cref="IUserAuthorisationContextFactory"/> type for
+        /// loading a <see cref="IUserAuthorisationContext" /> that will be available to handlers for authenticated users.
+        /// </summary>
+        /// <remarks>
+        /// Three new middleware builders will be added:
+        /// <list type="bullet">
+        /// <item><description><see cref="AuthenticationMiddlewareBuilder"/> added to <see cref="MiddlewareStage.Authentication"/>.</description></item>
+        /// <item><description><see cref="UserContextLoaderMiddlewareBuilder"/> added to <see cref="MiddlewareStage.Authorisation"/>.</description></item>
+        /// <item><description><see cref="AuthorisationMiddlewareBuilder"/> added to <see cref="MiddlewareStage.Authorisation"/>.</description></item>
+        /// </list>
+        /// </remarks>
+        /// <param name="middlewareConfigurer">The configurer to add auth to.</param>
+        /// <typeparam name="T">The type used as a user auth context factory.</typeparam>
+        /// <returns>The configurer.</returns>
+        public static BlueprintMiddlewareConfigurer AddAuth<T>(this BlueprintMiddlewareConfigurer middlewareConfigurer) where T : class, IUserAuthorisationContextFactory
         {
-            AddAuthServices(middlewareConfigurer);
+            middlewareConfigurer.Services.AddScoped<IUserAuthorisationContextFactory, T>();
+
+            TryAddAuthServices(middlewareConfigurer);
 
             middlewareConfigurer.AddMiddleware<AuthenticationMiddlewareBuilder>(MiddlewareStage.Authentication);
             middlewareConfigurer.AddMiddleware<UserContextLoaderMiddlewareBuilder>(MiddlewareStage.Authorisation);
@@ -77,7 +111,7 @@ namespace Blueprint.Api.Configuration
         public static BlueprintMiddlewareConfigurer AddHateoasLinks(this BlueprintMiddlewareConfigurer middlewareConfigurer)
         {
             // Resource events needs authoriser services to be registered
-            AddAuthServices(middlewareConfigurer);
+            TryAddAuthServices(middlewareConfigurer);
 
             middlewareConfigurer.Services.TryAddSingleton<IApiLinkGenerator, ApiLinkGenerator>();
             middlewareConfigurer.Services.TryAddScoped<IResourceLinkGenerator, EntityOperationResourceLinkGenerator>();
@@ -96,7 +130,7 @@ namespace Blueprint.Api.Configuration
             return middlewareConfigurer;
         }
 
-        private static void AddAuthServices(BlueprintMiddlewareConfigurer middlewareConfigurer)
+        private static void TryAddAuthServices(BlueprintMiddlewareConfigurer middlewareConfigurer)
         {
             middlewareConfigurer.Services.TryAddSingleton<IApiAuthoriserAggregator, ApiAuthoriserAggregator>();
             middlewareConfigurer.Services.TryAddSingleton<IClaimInspector, ClaimInspector>();
