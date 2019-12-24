c58efa3b50c7a4205915d639b0f74fd8cdae207e
Adam Barclay <barclay.adam@gmail.com>
2019-12-24 12:35:00 +0000

    [Notifications] Enable registering assemblies to scan for embedded notifications

--- src/Blueprint.Notifications/BlueprintConfigurerExtensions.cs
+++ src/Blueprint.Notifications/BlueprintConfigurerExtensions.cs
@@ -1,21 +1,29 @@
-﻿using Blueprint.Api.Configuration;
+﻿using System.Reflection;
+using Blueprint.Api.Configuration;
 using Blueprint.Notifications.Handlers;
 using Blueprint.Notifications.Templates;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Blueprint.Notifications
 {
     public static class BlueprintConfigurerExtensions
     {
-        public static BlueprintApiConfigurer AddNotifications(this BlueprintApiConfigurer blueprintApiConfigurer)
+        public static BlueprintApiConfigurer AddNotifications(this BlueprintApiConfigurer blueprintApiConfigurer, params Assembly[] embeddedResourceAssemblies)
         {
+            if (embeddedResourceAssemblies.Length == 0)
+            {
+                embeddedResourceAssemblies = new[] {Assembly.GetEntryAssembly()};
+            }
+
             blueprintApiConfigurer.Services.AddOptions<TemplatedEmailHandlerOptions>();
 
-            blueprintApiConfigurer.Services.AddTransient<INotificationRepository, EmbeddedResourceNotificationRepository>();
+            blueprintApiConfigurer.Services.AddTransient<INotificationRepository>(p =>
+                new EmbeddedResourceNotificationRepository(embeddedResourceAssemblies, p.GetRequiredService<ILogger<EmbeddedResourceNotificationRepository>>()));
+
             blueprintApiConfigurer.Services.AddTransient<INotificationService, NotificationService>();
             blueprintApiConfigurer.Services.AddTransient<INotificationHandler, TemplatedEmailHandler>();
             blueprintApiConfigurer.Services.AddTransient<ITemplateFactory, NVelocityTemplateFactory>();
-            blueprintApiConfigurer.Services.AddTransient<INotificationRepository, EmbeddedResourceNotificationRepository>();
             blueprintApiConfigurer.Services.AddTransient<IEmailSender, SmtpClientEmailSender>();
 
             return blueprintApiConfigurer;
