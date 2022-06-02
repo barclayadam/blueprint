afc38100eea98cb3331c36ee1a3b6a4b349bf419
Adam Barclay <barclay.adam@gmail.com>
2022-06-02 19:20:39 +0000

    [Docs] Document ConstructorFrame and friends

--- src/Blueprint.Compiler/Frames/ConstructorFrame.cs
+++ src/Blueprint.Compiler/Frames/ConstructorFrame.cs
@@ -9,14 +9,13 @@ using Blueprint.Compiler.Util;
 
 namespace Blueprint.Compiler.Frames
 {
+    /// <summary>
+    /// A frame that is used to call a constructor to create a new instance
+    /// of a class.
+    /// </summary>
     public class ConstructorFrame : SyncFrame
     {
-        public ConstructorFrame(ConstructorInfo ctor)
-            : this(ctor.DeclaringType, ctor)
-        {
-        }
-
-        public ConstructorFrame(Type builtType, ConstructorInfo ctor)
+        internal ConstructorFrame(Type builtType, ConstructorInfo ctor)
         {
             this.Ctor = ctor ?? throw new ArgumentNullException(nameof(ctor));
             this.Parameters = new Variable[ctor.GetParameters().Length];
@@ -25,18 +24,50 @@ namespace Blueprint.Compiler.Frames
             this.Variable = new Variable(this.BuiltType, this);
         }
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ConstructorFrame" />
+        /// using the given <see cref="ConstructorInfo" />
+        /// </summary>
+        /// <param name="ctor">The constructor to be called.</param>
+        public ConstructorFrame(ConstructorInfo ctor)
+            : this(ctor.DeclaringType, ctor)
+        {
+        }
+
+        /// <summary>
+        /// The type being built.
+        /// </summary>
         public Type BuiltType { get;  }
 
-        public Type DeclaredType { get; set; }
+        /// <summary>
+        /// An optional type that can be used to specify the type of the
+        /// variable created, instead of using <c>var</c> (i.e. if this is set
+        /// the output will be <c>[DeclaringType] [VariableName] = new [CtorCall]</c>).
+        /// </summary>
+        public Type? DeclaredType { get; set; }
 
-        public ConstructorInfo Ctor { get; }
+        /// <summary>
+        /// The acual constructor to be used.
+        /// </summary>
+        internal ConstructorInfo Ctor { get; }
 
+        /// <summary>
+        /// The parameters to the constructor, matching the positional
+        /// arguments.
+        /// </summary>
         public Variable[] Parameters { get; }
 
         public FramesCollection ActivatorFrames { get; } = new FramesCollection();
 
+        /// <summary>
+        /// Determines the way the constructor call will be output.
+        /// </summary>
         public ConstructorCallMode Mode { get; set; } = ConstructorCallMode.Variable;
 
+        /// <summary>
+        /// A list of setters that can be used to set properties of the
+        /// created instance.
+        /// </summary>
         public IList<SetterArg> Setters { get; } = new List<SetterArg>();
 
         /// <summary>
@@ -99,6 +130,12 @@ namespace Blueprint.Compiler.Frames
             }
         }
 
+        /// <summary>
+        /// The declaration code, which includes the setting of the <see cref="Variable" />
+        /// (using <c>var</c> or the <see cref="DeclaredType" />) plus the actual
+        /// invocation of the constructor.
+        /// </summary>
+        /// <returns>The code used to set a variable with result of this constructor call.</returns>
         public string Declaration()
         {
             return this.DeclaredType == null
@@ -106,14 +143,20 @@ namespace Blueprint.Compiler.Frames
                 : $"{this.DeclaredType.FullNameInCode()} {this.Variable} = {this.Invocation()}";
         }
 
+        /// <summary>
+        /// The invocation of this constructor (i.e. the <c>new</c> construct), plus
+        /// any setters.
+        /// </summary>
+        /// <returns>The constructor invocation.</returns>
         public string Invocation()
         {
-            var tempQualifier = this.Parameters.Select(x => x.Usage);
-            var invocation = $"new {this.BuiltType.FullNameInCode()}({string.Join(", ", tempQualifier)})";
+            var arguments = this.Parameters.Select(x => x.Usage);
+            var invocation = $"new {this.BuiltType.FullNameInCode()}({string.Join(", ", arguments)})";
+
             if (this.Setters.Any())
             {
-                var tempQualifier1 = this.Setters.Select(x => x.Assignment());
-                invocation += $"{{{string.Join(", ", tempQualifier1)}}}";
+                var setterInvocations = this.Setters.Select(x => x.Assignment());
+                invocation += $"{{{string.Join(", ", setterInvocations)}}}";
             }
 
             return invocation;
@@ -128,18 +171,37 @@ namespace Blueprint.Compiler.Frames
         }
     }
 
+    /// <summary>
+    /// A typed <see cref="ConstructorFrame" />.
+    /// </summary>
+    /// <typeparam name="T">The built type.</typeparam>
     public class ConstructorFrame<T> : ConstructorFrame
     {
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ConstructorFrame{T}" />
+        /// class from the given constructor info.
+        /// </summary>
+        /// <param name="ctor">The constructor info.</param>
         public ConstructorFrame(ConstructorInfo ctor)
             : base(typeof(T), ctor)
         {
         }
 
+        /// <summary>
+        /// Initialises a new instance of the <see cref="ConstructorFrame{T}" />
+        /// class by an Expression that represents a constructor call.
+        /// </summary>
+        /// <param name="expression">An expression that represents a constructor call.</param>
         public ConstructorFrame(Expression<Func<T>> expression)
             : base(typeof(T), ConstructorFinderVisitor<T>.Find(expression))
         {
         }
 
+        /// <summary>
+        /// Sets a property, at construction time, to the result of the given variable.
+        /// </summary>
+        /// <param name="expression">An expression that is a property reference (i.e. t => t.[PropertyName]).</param>
+        /// <param name="variable">The variable to set the property tp.</param>
         public void Set(Expression<Func<T, object>> expression, Variable variable = null)
         {
             var property = ReflectionHelper.GetProperty(expression);
